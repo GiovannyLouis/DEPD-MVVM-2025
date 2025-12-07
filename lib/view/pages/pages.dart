0a128b747d36530b5a46f4fd4ba85f3228933832
Louis <louis.anth10@gmail.com>
2025-12-07 23:21:01 +0700

    project flutter

--- lib/view/pages/pages.dart
+++ lib/view/pages/pages.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:depd_mvvm_2025/view/widgets/widgets.dart';
 import 'package:depd_mvvm_2025/viewmodel/home_viewmodel.dart';
+import 'package:depd_mvvm_2025/viewmodel/international_viewmodel.dart';
 import 'package:depd_mvvm_2025/data/response/status.dart';
 
 part 'home_page.dart';
+part 'international_page.dart';
+part 'bonus_page.dart';
+part 'main_page.dart';
--- lib/view/widgets/widgets.dart
+++ lib/view/widgets/widgets.dart
@@ -3,4 +3,5 @@ import 'package:intl/intl.dart';
 import 'package:depd_mvvm_2025/model/model.dart';
 
 part 'card_cost.dart';
+part 'card_international_cost.dart';
 part 'bottom_sheets_cost.dart';
